0d4ea2acee2b8d3c322f472dfd2f56bd7a25902a
Colin_31 <13764098996@163.com>
2025-10-31 14:57:37 +0800

    更新Readme，发布1.1.0

--- app/src/main/java/com/aiprojects/gameai/SettingActivity.kt
+++ app/src/main/java/com/aiprojects/gameai/SettingActivity.kt
@@ -5,7 +5,6 @@ import android.content.ClipboardManager
 import android.widget.Toast
 import android.content.Context
 import android.graphics.Color
-import android.graphics.PorterDuff
 import android.os.Bundle
 import android.util.AttributeSet
 import android.util.Log
